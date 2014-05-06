94e19e0cfa9d469686dab03ddce315b9527de271
ArkadiuszD <aarkadiusz.dabrowski@gmail.com>
2014-05-06 23:04:23 +0200

    Do Sprawdzenia
    Na szybko bez sprawdzania,
    
    Do Sprawdzenia

--- test/helpers/notes_helper_test.rb
+++ test/helpers/notes_helper_test.rb
@@ -2,3 +2,7 @@ require 'test_helper'
 
 class NotesHelperTest < ActionView::TestCase
 end
+
+
+# Niech ktoś to przejrzy wydaje mi się że tu coś będzie
+#https://github.com/stffn/declarative_authorization/blob/master/test/controller_test.rb
